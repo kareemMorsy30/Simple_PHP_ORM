29905fec5591ffcd4efd955749d250c123632d2e
Kareem Saeed <kareemmorsy30@gmail.com>
2021-08-10 20:32:40 +0200

    Add two helper methods to helper class to get country code and check code validity of a phone number

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -3,6 +3,14 @@
 namespace App\Helpers;
 
 class Helper {
+    private const CODE_MAPPER = [
+        '237' => 'Cameroon',
+        '251' => 'Ethiopia',
+        '212' => 'Morocco',
+        '258' => 'Mozambique',
+        '256' => 'Uganda',
+    ];
+
     public static function getCountryCodeOfPhone($phone) {
         if ($phone) {
             $phone = explode(' ', $phone);
@@ -10,5 +18,32 @@ class Helper {
         }
 
         return null;
+    }
+
+    public static function getCountryByCode($code) {
+        return self::CODE_MAPPER[$code];
+    }
+
+    public static function checkCodeValidity($phone) {
+        $code = self::getCountryCodeOfPhone($phone);
+        $country = self::getCountryByCode($code);
+
+        switch($country) {
+            case 'Cameroon':
+                return preg_match("/\(237\)\ ?[2368]\d{7,8}$/", $phone);
+                break;
+            case 'Ethiopia':
+                return preg_match("/\(251\)\ ?[1-59]\d{8}$/", $phone);
+                break;
+            case 'Morocco':
+                return preg_match("/\(212\)\ ?[5-9]\d{8}$/", $phone);
+                break;
+            case 'Mozambique':
+                return preg_match("/\(258\)\ ?[28]\d{7,8}$/", $phone);
+                break;
+            case 'Uganda':
+                return preg_match("/\(256\)\ ?\d{9}$/", $phone);
+                break;
+        }
     }
 }
