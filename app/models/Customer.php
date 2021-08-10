969b6b5fcc4826c8817483fee084ea73888e095b
Kareem Saeed <kareemmorsy30@gmail.com>
2021-08-10 20:34:18 +0200

    Add a new appended field "state" to customer model to get the state of each phone number (valid or not)

--- app/models/Customer.php
+++ app/models/Customer.php
@@ -10,15 +10,16 @@ class Customer extends Model {
     protected $appends = [
         'countryCode',
         'phoneNum',
-        'country'
+        'country',
+        'state'
     ];
 
-    private const CODE_MAPPER = [
-        '237' => 'Cameroon',
-        '251' => 'Ethiopia',
-        '212' => 'Morocco',
-        '258' => 'Mozambique',
-        '256' => 'Uganda',
+    private const COUNTRY_REGEX_MAPPER = [
+        'Cameroon' => 'Cameroon',
+        'Ethiopia' => 'Ethiopia',
+        'Morocco' => 'Morocco',
+        'Mozambique' => 'Mozambique',
+        'Uganda' => 'Uganda',
     ];
 
     protected function getCountryCodeAttribute() {
@@ -41,7 +42,17 @@ class Customer extends Model {
     protected function getCountryAttribute() {
         if ($this->phone) {
             $country_code = Helper::getCountryCodeOfPhone($this->phone);
-            return self::CODE_MAPPER[$country_code];
+            return Helper::getCountryByCode($country_code);
+        }
+        
+        return null;
+    }
+
+    public function getStateAttribute() {
+        if ($this->phone) {
+            $state = Helper::checkCodeValidity($this->phone);
+
+            return $state ? 'OK' : 'NOK';
         }
         
         return null;
