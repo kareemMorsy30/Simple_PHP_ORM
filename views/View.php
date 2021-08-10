ee7ab59f509d701c44460c55d241464e73cc1491
Kareem Saeed <kareemmorsy30@gmail.com>
2021-08-10 20:36:58 +0200

    Return default three variables to any project views that indicate current page, next and previous page links

--- views/View.php
+++ views/View.php
@@ -2,6 +2,7 @@
 
 namespace Views;
 
+use App\Requests\Request;
 class View
 {
     private $data = array();
@@ -19,6 +20,16 @@ class View
                 foreach($variables as $variable => $value) {
                     $this->data[$variable] = $value;
                 }
+
+                $request = new Request();
+                $page = isset($request->page) ? $request->page : 1;
+                $next_page = $page + 1;
+                $previous_page = $page == 1 ? 1 : $page - 1;
+                $next_page_link = "http://$_SERVER[HTTP_HOST]".strtok($_SERVER['REQUEST_URI'], '?')."?page=".$next_page;
+                $previous_page_link = "http://$_SERVER[HTTP_HOST]".strtok($_SERVER['REQUEST_URI'], '?')."?page=".$previous_page;
+                $this->data['current_page'] = $page;
+                $this->data['next_page_link'] = $next_page_link;
+                $this->data['previous_page_link'] = $previous_page_link;
             } else {
                 throw new \Exception('Template ' . $template . ' not found!');
             }
