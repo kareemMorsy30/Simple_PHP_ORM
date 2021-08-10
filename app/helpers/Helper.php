31235d91a6c0ecdf0ae86a87abbc7a9a619d0a3e
Kareem Saeed <kareemmorsy30@gmail.com>
2021-08-10 23:07:00 +0200

    Add an associative array to map codes to countries and its REGEX in Helper class

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -4,12 +4,31 @@ namespace App\Helpers;
 
 class Helper {
     private const CODE_MAPPER = [
-        '237' => 'Cameroon',
-        '251' => 'Ethiopia',
-        '212' => 'Morocco',
-        '258' => 'Mozambique',
-        '256' => 'Uganda',
+        '237' => [
+            'country' => 'Cameroon',
+            'regex' => "\(237\)\ ?[2368]\d{7,8}$"
+        ],
+        '251' => [
+            'country' => 'Ethiopia',
+            'regex' => "\(251\)\ ?[1-59]\d{8}$"
+        ],
+        '212' => [
+            'country' => 'Morocco',
+            'regex' => "\(212\)\ ?[5-9]\d{8}$"
+        ],
+        '258' => [
+            'country' => 'Mozambique',
+            'regex' => "\(258\)\ ?[28]\d{7,8}$"
+        ],
+        '256' => [
+            'country' => 'Uganda',
+            'regex' => "\(256\)\ ?\d{9}$"
+        ]
     ];
+
+    public static function getCodes() {
+        return self::CODE_MAPPER;
+    }
 
     public static function getCountryCodeOfPhone($phone) {
         if ($phone) {
@@ -21,29 +40,11 @@ class Helper {
     }
 
     public static function getCountryByCode($code) {
-        return self::CODE_MAPPER[$code];
+        return self::CODE_MAPPER[$code]['country'];
     }
 
     public static function checkCodeValidity($phone) {
         $code = self::getCountryCodeOfPhone($phone);
-        $country = self::getCountryByCode($code);
-
-        switch($country) {
-            case 'Cameroon':
-                return preg_match("/\(237\)\ ?[2368]\d{7,8}$/", $phone);
-                break;
-            case 'Ethiopia':
-                return preg_match("/\(251\)\ ?[1-59]\d{8}$/", $phone);
-                break;
-            case 'Morocco':
-                return preg_match("/\(212\)\ ?[5-9]\d{8}$/", $phone);
-                break;
-            case 'Mozambique':
-                return preg_match("/\(258\)\ ?[28]\d{7,8}$/", $phone);
-                break;
-            case 'Uganda':
-                return preg_match("/\(256\)\ ?\d{9}$/", $phone);
-                break;
-        }
+        return preg_match('/'.self::CODE_MAPPER[$code]['regex'].'/', $phone);
     }
 }
